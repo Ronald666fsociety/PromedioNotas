63c39fbf35f0dd37de0114b01223eeadaea798b7
Juan Perez <juan@email.com>
2026-07-20 21:57:39 -0400

    Versión inicial del sistema de promedios

---
+++ app.js
@@ -0,0 +1,16 @@
+// ===================================
+// Sistema de Promedio de Notas
+// ===================================
+
+const nota1 = 70;
+const nota2 = 85;
+const nota3 = 90;
+
+const promedio = (nota1 + nota2 + nota3) / 3;
+
+console.log("Sistema de Promedios");
+console.log("\t");
+console.log("Nota 1:", nota1);
+console.log("Nota 2:", nota2);
+console.log("Nota 3:", nota3);
+console.log("Promedio:", promedio.toFixed(2));
